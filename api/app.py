1f5463bbaac32606133f8cfbac15cd6e45967072
藤野 宏樹 <fujino@rss0041npc.local>
2018-03-21 15:38:47 +0900

    crossドメイン対応

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 # -*-coding:utf-8-*-
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from block_chain import BlockChain
 
 # Instantiate the Node
 app = Flask(__name__)
+CORS(app)
 
 block_chain = BlockChain()
 
