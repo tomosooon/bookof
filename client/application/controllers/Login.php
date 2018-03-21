816e8a5f2830afcbffa60a0ff269acd22fc67019
k-a-o-r-u <chewbacca200_walkingc@yahoo.co.jp>
2018-03-21 15:49:14 +0900

    add session

---
+++ client/application/controllers/Login.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        //$this->load->helper('url');
+        //$this->load->library('session');
+    }
+
+    public function index()
+    {
+        $this->load->helper('url');
+
+        $data['title'] = 'login';
+        $email = $this->input->post('email') != '' ? $this->input->post('email') : '';
+        $send = $this->input->post('send') != '' ? $this->input->post('send') : '';
+        if ($send != '') {
+            $_SESSION['email'] = $email;
+            $url = "http://".base_url()."home";
+            header("Location: {$url}");
+            exit();
+
+        }
+
+        $this->load->view('header.php',$data);
+        $this->load->view('login/index',$data);
+    }
+}
